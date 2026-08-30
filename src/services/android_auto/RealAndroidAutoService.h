a03babee126f85d0d8dbd305aa91112c2d6de6be
beegormif <a.stesmith1@gmail.com>
2026-08-30 20:35:06 +0100

    more debug

--- src/services/android_auto/RealAndroidAutoService.h
+++ src/services/android_auto/RealAndroidAutoService.h
@@ -180,7 +180,9 @@ class RealAndroidAutoService : public AndroidAutoService {
 
   bool setDisplayResolution(const QSize& resolution) override;
   [[nodiscard]] auto getNegotiatedVideoResolution() const -> QSize override { return m_negotiatedVideoResolution; }
-  [[nodiscard]] auto getNegotiatedTouchResolution() const -> QSize { return m_negotiatedTouchResolution; }
+  [[nodiscard]] auto getNegotiatedTouchResolution() const -> QSize {
+  return negotiatedVideoResolution();
+}
   auto setNegotiatedTouchResolution(const QSize& resolution) -> void override {
     m_negotiatedTouchResolution = resolution;
   }
